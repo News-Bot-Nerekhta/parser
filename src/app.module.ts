37478b517d2ff63bc93a6f8ff8be1e2dde5a161c
DaniilL12321 <lobanov006@gmail.com>
2025-04-11 22:31:56 +0300

    chore(database): add example .env and change default DB name

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { NewsModule } from './news/news.module';
       port: parseInt(process.env.DB_PORT || '5432'),
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_DATABASE || 'newsdb',
+      database: process.env.DB_DATABASE || 'postgres',
       autoLoadEntities: true,
       synchronize: true,
       ssl: {
